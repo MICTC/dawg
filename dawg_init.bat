e61cd5e0b0fb3cb05288b7c3a494f8e3ddce6807
MikeEhman <nkim412@gmail.com>
2013-05-23 17:06:47 -0500

    added the init batch file

---
+++ dawg_init.bat
@@ -0,0 +1,8 @@
+call git init
+call git pull git@github.com:MedInfoCorp/dawg.git master
+call git branch develop
+call git checkout develop
+call git pull git@github.com:MedInfoCorp/dawg.git develop
+call git branch feature
+call git checkout feature
+call git pull git@github.com:MedInfoCorp/dawg.git feature
